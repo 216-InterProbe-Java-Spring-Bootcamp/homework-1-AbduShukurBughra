3d37fece648284166d805670ffe942aecead9a10
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-06-25 18:18:56 +0300

    spring homework one

---
+++ interprobe_bootcamp_homework/src/main/java/com/abdushukur/interprobe_bootcamp_homework/InterprobeBootcampHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.abdushukur.interprobe_bootcamp_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterprobeBootcampHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterprobeBootcampHomeworkApplication.class, args);
+    }
+
+}
---
+++ interprobe_bootcamp_homework/src/test/java/com/abdushukur/interprobe_bootcamp_homework/InterprobeBootcampHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdushukur.interprobe_bootcamp_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterprobeBootcampHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
